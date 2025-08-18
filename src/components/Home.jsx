fe42e3d9e11122b393484741044cfc3930ac6c61
Mrunmai <maiswati18@gmail.com>
2025-08-18 10:06:59 +0530

    Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import mukta from '../assets/Mukta 2.jpg' 
-import '../index.css'
+import mukta from "../assets/Mukta 2.jpg";
+import "../index.css";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div 
+    <div
       className="hero-section d-flex align-items-center justify-content-center text-center text-light"
       style={{
         backgroundImage: `url(${mukta})`,
@@ -17,15 +17,15 @@ const Home = () => {
         position: "relative",
       }}
     >
-      <div 
-        className="overlay" 
+      <div
+        className="overlay"
         style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
           height: "100%",
-          backgroundColor: "rgba(0,0,0,0.65)", 
+          backgroundColor: "rgba(0,0,0,0.65)",
           zIndex: 1,
         }}
       ></div>
@@ -33,22 +33,24 @@ const Home = () => {
       <div style={{ zIndex: 2 }}>
         <h1 className="cinzel">Mukta Barve</h1>
         <p className="cinzel">Actress | Dreamer | Storyteller</p>
-        <NavLink to='/showreel' style={{textDecoration:"none"}}>
- <button
-          className="btn cinzel-600 m-2"
-          style={{ backgroundColor: "#F7DAC0" }}
-        >
-          Showreel
-        </button>
+        <NavLink to="/showreel" style={{ textDecoration: "none" }}>
+          <button
+            className="btn cinzel-600 m-2"
+            style={{ backgroundColor: "#F7DAC0" }}
+          >
+            Showreel
+          </button>
         </NavLink>
-       
-        <button
-          className="btn cinzel-600"
-          style={{ backgroundColor: "#F7DAC0" }}
-        >
-          Portfolio
-        </button>
-        <NavLink to='/upcoming-projects'  style={{textDecoration:"none"}}>
+        <NavLink to="/about" style={{ textDecoration: "none" }}>
+          <button
+            className="btn cinzel-600"
+            style={{ backgroundColor: "#F7DAC0" }}
+          >
+            Portfolio
+          </button>
+        </NavLink>
+
+        <NavLink to="/upcoming-projects" style={{ textDecoration: "none" }}>
           <p className="cinzel text-white">Upcoming Projects</p>
         </NavLink>
       </div>
