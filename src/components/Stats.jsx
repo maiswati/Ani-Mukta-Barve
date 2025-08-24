ad3c7e65153056be209782d49efc59d74a30fe95
Mrunmai <maiswati18@gmail.com>
2025-08-24 10:04:59 +0530

    Update

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,8 +8,8 @@ const Stats = () => {
     <div className='container-fluid' style={{backgroundColor:"#FEFBF8", minHeight: "100vh"}}>
           <div className="row m-5">
             <h1 className='cinzel text-center mb-5 mt-4' style={{textDecoration:"underline"}} >Stats</h1>
-            <div className="col-lg-4 text-center">
-                <img src={movie} alt="movie" className='img-fluid mb-4' style={{width:"150px"}}/>
+            <div className="col-lg-4 text-center mb-4">
+                <img src={movie} alt="movie" className='img-fluid mb-4' style={{width:"100px"}}/>
                 <div>
                     <h1 className='space-mono-bold'>
                         <CountUp end={42} duration={4}/> +
@@ -17,8 +17,8 @@ const Stats = () => {
                     <h3 className='space-mono-bold'>Films</h3>
                 </div>
             </div>
-            <div className="col-lg-4 text-center">
-                <img src={tv} alt="movie" className='img-fluid mb-4' style={{width:"110px"}}/>
+            <div className="col-lg-4 text-center mb-4 ">
+                <img src={tv} alt="movie" className='img-fluid mb-4' style={{width:"75px"}}/>
                 <div>
                     <h1 className='space-mono-bold'>
                         <CountUp end={20} duration={4}/> +
@@ -26,8 +26,8 @@ const Stats = () => {
                     <h3 className='space-mono-bold'>TV Shows</h3>
                 </div>
             </div>
-            <div className="col-lg-4 text-center">
-                <img src={theatre} alt="movie" className='img-fluid mb-4' style={{width:"185px"}}/>
+            <div className="col-lg-4 text-center mt-4">
+                <img src={theatre} alt="movie" className='img-fluid mb-4' style={{width:"110px"}}/>
                 <div>
                     <h1 className='space-mono-bold'>
                         <CountUp end={17} duration={4}/> +
