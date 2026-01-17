86d719ec5e576119246288b57b18c4c145f2289a
Mrunmai <maiswati18@gmail.com>
2026-01-17 17:16:05 +0530

    Added new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,16 +36,6 @@ const Home = () => {
         </h1>
         <p className="cinzel" style={{color:"blanchedalmond", fontSize:20}}>Actress | Dreamer | Storyteller</p>
         <br /><br />
-        <h4
-          style={{
-            color: "#A0522D",
-            textShadow: "0 0 3px #F5DEB3, 0 0 12px #8B4513",
-            fontWeight: "bold",
-          }}
-        >
-          कन्या ऐसी देई , जैसी मीरा आणि{" "}
-            <h2><b>मुक्ताबाई</b></h2>
-        </h4>
         <NavLink to="/showreel" style={{ textDecoration: "none" }}>
           <button
             className="btn cinzel-600 m-2"
