b93d05e6406dd607ddc17e2514b386de9d1ae2f9
Mrunmai <maiswati18@gmail.com>
2026-01-17 13:49:31 +0530

    Added maya

--- src/components/UpcomingProjects.jsx
+++ src/components/UpcomingProjects.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import asambhav from '../assets/asambhav.png'
 import ravsaheb from '../assets/raavsaheb.webp'
 import mpm4 from '../assets/mpmm4.jpeg'
-import maya from '../assets/maya.jpeg'
+import maya2 from '../assets/mayamuk.jpeg'
+
 const UpcomingProjects = () => {
   return (
     <div className='container-fluid' style={{backgroundColor:"#1C1B1B", minHeight: "100vh"}}>
@@ -10,7 +11,7 @@ const UpcomingProjects = () => {
             <h1 className='cinzel text-center text-light mb-5 mt-4' style={{textDecoration:"underline"}} >Upcoming Projects</h1>
             <div className='d-flex gap-5 flex-wrap'>
               <div className='border border-3 borderColor p-4 text-center' style={{width:"300px"}}>
-                <img src={maya} alt="mpm4" className='img-fluid mb-3' />
+                <img src={maya2} alt="mpm4" className='img-fluid mb-3' />
                 <h1 className='space-mono-regular asambhav'>Maya</h1>
                 <p className='space-mono-regular asambhav'>Release Date : 27th Feb 2026</p>
                 <button className='btn space-mono-regular' style={{ backgroundColor: "#F7DAC0" }}>Show Details</button>
