905a9850b10380d0fc92342d8d1a56122e41ee23
Mrunmai <maiswati18@gmail.com>
2026-01-20 10:59:35 +0530

    Intial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import mukta from "../assets/Mukta 2.jpg";
 import mukta2 from '../assets/about-section-image.png'
 import "../index.css";
+import muktaTai from '../assets/muktaTai.jpeg'
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
     <div
       className="hero-section d-flex align-items-center justify-content-center text-center text-light"
       style={{
-        backgroundImage: `url(${mukta2})`,
+        backgroundImage: `url(${muktaTai})`,
         backgroundSize: "cover",
         backgroundPosition: "center top",
         minHeight: "100vh",
@@ -25,21 +26,31 @@ const Home = () => {
           left: 0,
           width: "100%",
           height: "100%",
-          background: "linear-gradient(to right, rgba(0,0,0,0.7) 20%, rgba(0,0,0,0.1) 60%, rgba(0,0,0,0.1) 100%)",
           zIndex: 1,
         }}
       ></div>
-
+      <div
+  className="vignette-overlay"
+  style={{
+    position: "absolute",
+    inset: 0,
+    zIndex: 1,
+    pointerEvents: "none",
+  }}
+></div>
       <div style={{ zIndex: 2 }}>
-        <h1 className="cinzel" style={{fontWeight:700, color:"beige", fontSize:40}}>
-          <b>Mukta Barve</b>
+        <h1 className="cinzel hero-animate hero-title" style={{fontWeight:700, color:"beige", fontSize:40, textShadow:"0 3px 6px rgba(0, 0, 0, 0.6), 0 12px 28px rgba(0,0,0,0.45)"}}>
+          Mukta Barve
         </h1>
-        <p className="cinzel" style={{color:"blanchedalmond", fontSize:20}}>Actress | Dreamer | Storyteller</p>
+        <p className="cinzel hero-animate hero-subtitle" style={{color:"blanchedalmond", fontSize:20, opacity:0.95,  textShadow: "0 2px 6px rgba(0,0,0,0.45)"}}>Actress | Dreamer | Storyteller</p>
         <br /><br />
+        <div className="hero-animate hero-buttons">
         <NavLink to="/showreel" style={{ textDecoration: "none" }}>
           <button
+            aria-label="Watch Mukta Barve Showreel"
             className="btn cinzel-600 m-2"
-            style={{ backgroundColor: "#F7DAC0" }}
+            style={{ backgroundColor: "#F7DAC0",  boxShadow: "0 6px 18px rgba(40,25,10,0.45)",
+  border: "none", }}
           >
             Showreel
           </button>
@@ -47,11 +58,13 @@ const Home = () => {
         <NavLink to="/about" style={{ textDecoration: "none" }}>
           <button
             className="btn cinzel-600"
-            style={{ backgroundColor: "#F7DAC0" }}
+            style={{ backgroundColor: "#F7DAC0",  boxShadow: "0 6px 18px rgba(40,25,10,0.45)",
+  border: "none", }}
           >
             Portfolio
           </button>
         </NavLink>
+        </div>
 
         <NavLink to="/upcoming-projects" style={{ textDecoration: "none" }}>
           <p className="cinzel text-white">Upcoming Projects</p>
